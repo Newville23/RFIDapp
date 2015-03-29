f0a86f7a7f4b8dc6182bfd28311bdc839f13afce
newville23 <villanuevad@uninorte.edu.co>
2015-03-29 10:13:13 -0500

    init commit

---
+++ Inventory/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Finalizar = new System.Windows.Forms.Button();
+            this.iniciar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Item2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descripcion2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.total = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.tabla1 = new System.Windows.Forms.DataGridView();
+            this.Item = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ubicacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fuente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Finalizar);
+            this.groupBox1.Controls.Add(this.iniciar);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(161, 361);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Finalizar
+            // 
+            this.Finalizar.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Finalizar.Location = new System.Drawing.Point(23, 81);
+            this.Finalizar.Name = "Finalizar";
+            this.Finalizar.Size = new System.Drawing.Size(114, 33);
+            this.Finalizar.TabIndex = 1;
+            this.Finalizar.Text = "Finalizar";
+            this.Finalizar.UseVisualStyleBackColor = false;
+            this.Finalizar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // iniciar
+            // 
+            this.iniciar.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.iniciar.Location = new System.Drawing.Point(23, 28);
+            this.iniciar.Name = "iniciar";
+            this.iniciar.Size = new System.Drawing.Size(114, 33);
+            this.iniciar.TabIndex = 0;
+            this.iniciar.Text = "Iniciar";
+            this.iniciar.UseVisualStyleBackColor = false;
+            this.iniciar.Click += new System.EventHandler(this.iniciar_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(215, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(572, 146);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Total objetos";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Item2,
+            this.descripcion2,
+            this.total});
+            this.dataGridView1.Location = new System.Drawing.Point(123, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(344, 103);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // Item2
+            // 
+            this.Item2.HeaderText = "Item";
+            this.Item2.Name = "Item2";
+            // 
+            // descripcion2
+            // 
+            this.descripcion2.HeaderText = "Descripción";
+            this.descripcion2.Name = "descripcion2";
+            // 
+            // total
+            // 
+            this.total.HeaderText = "total";
+            this.total.Name = "total";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tabla1);
+            this.groupBox3.Location = new System.Drawing.Point(217, 164);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(570, 208);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            // 
+            // tabla1
+            // 
+            this.tabla1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tabla1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Item,
+            this.Descripcion,
+            this.Ubicacion,
+            this.Fuente});
+            this.tabla1.Location = new System.Drawing.Point(71, 19);
+            this.tabla1.Name = "tabla1";
+            this.tabla1.Size = new System.Drawing.Size(444, 234);
+            this.tabla1.TabIndex = 0;
+            this.tabla1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Item
+            // 
+            this.Item.HeaderText = "ITEM";
+            this.Item.Name = "Item";
+            // 
+            // Descripcion
+            // 
+            this.Descripcion.HeaderText = "Descripción";
+            this.Descripcion.Name = "Descripcion";
+            // 
+            // Ubicacion
+            // 
+            this.Ubicacion.HeaderText = "Ubicación";
+            this.Ubicacion.Name = "Ubicacion";
+            // 
+            // Fuente
+            // 
+            this.Fuente.HeaderText = "Fuente de adquisición";
+            this.Fuente.Name = "Fuente";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(827, 385);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "RFID-Inventory";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tabla1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Finalizar;
+        private System.Windows.Forms.Button iniciar;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView tabla1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Item;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descripcion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ubicacion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fuente;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Item2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descripcion2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn total;
+    }
+}
+
---
+++ Inventory/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using com.caen.RFIDLibrary;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApplication1
+{
+
+      public partial class Form1 : Form 
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           // davidreader.InventoryAbort();
+           // davidreader.Disconnect();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void iniciar_Click(object sender, EventArgs e)
+        {
+            DBConnect DB = new DBConnect();
+            CAENRFIDReader davidreader = new CAENRFIDReader();
+            davidreader.CAENRFIDEvent += new CAENRFIDEventHandler(davidreader_EventHandler);
+            CAENRFIDLogicalSource LS0;
+            byte[] Mask = new byte[4];
+
+
+
+
+            //public event CAENRFIDEventHandler davidreader_EventHandler;
+            davidreader.Connect(CAENRFIDPort.CAENRFID_TCP, "192.168.10.1");
+
+            LS0 = davidreader.GetSource("Source_0"); // punto de lectura
+
+            LS0.SetReadCycle(0);
+            //LS0.InventoryTag(Mask, 0x0, 0x0, 0x06);
+            LS0.EventInventoryTag(Mask, 0x0, 0x0, 0x06); //ciclos de lectura continuos el metodo arroja un bool 
+
+            Console.WriteLine("Inicia inventario");
+            DB.Insert("ABCD", "", "", "position");
+
+        }
+  
+
+       static void davidreader_EventHandler(object Sender, CAENRFIDEventArgs Event)
+        {
+            CAENRFIDNotify[] Tags = Event.Data; // Class that return a struct like { byte ID[MAX_ID_LENGTH]; short Length; char LogicalSource[MAX_LOGICAL_SOURCE_NAME]; char ReadPoint[MAX_READPOINT_NAME]; CAENRFIDProtocol Type; short RSSI; byte TID[MAX_TID_SIZE]; short TIDLen; byte XPC[XPC_LENGTH]; byte PC[PC_LENGTH];}
+            
+            DBConnect DB = new DBConnect();
+             
+
+            if (Tags.Length > 0)
+            {
+
+                for (int i = 0; i < Tags.Length; i++)
+                {
+                    String tag_id = BitConverter.ToString(Tags[i].getTagID());
+                    DateTime tag_time = Tags[i].getDate();
+                    string tag_source = Tags[i].getTagSource();
+
+                    
+
+                    if (DB.Select(tag_id) != null)// si el tag esta en la tabla
+                    {
+
+                        Console.WriteLine("entre");
+                    }
+                    else {
+
+                        //Escribiendo un tag nuevo en la tabla tags 
+                        DB.Insert(tag_id, tag_time.ToString(), tag_source, "position");
+                    }
+
+                    
+                    
+                  //hacer select para saber si el tag se encuentra guardado o no en la tabla
+                   
+                    //Luego hacer un insert si el return da empty  
+
+                    Console.WriteLine(tag_id);            
+                                                           
+                }
+            }
+
+
+
+        }
+      
+      
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+        
+        }
+
+     }
+ 
+
+    class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //Constructor
+        public DBConnect()
+        {
+            Initialize();
+        }
+
+        //Initialize values
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "test";
+            uid = "root";
+            password = "";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("No se logro conexión con el servidor");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        //Insert statement
+        public void Insert(string id, string time, string source, string position)
+        {
+            string query = "INSERT INTO tags (id, time, position, source) VALUES ('" + id + "' , '" + time + "', '" + position + "', '" + source + "')";
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+        //Select statement
+        public string Select(string id)
+        {
+            string tagreader = null;
+            string query = "SELECT id FROM tags WHERE id = '" + id + "'";
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    tagreader = reader.GetString(0);
+                    //Console.WriteLine("Aqui");
+                }
+
+                reader.Close();
+
+                //close connection
+                this.CloseConnection();
+
+            }
+            return tagreader;
+        }
+
+
+    }
+
+
+
+}
